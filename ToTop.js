b450a69ab57459a1fa1043018551b18e5bc55c48
unknown <slavabala2002@gmail.com>
2019-07-16 11:58:53 +0100

    CSS 13

---
+++ ToTop.js
@@ -0,0 +1,15 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    document.getElementById("toTop").style.display = "block";
+    } else {
+    document.getElementById("toTop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
